deb2cfe8d2fc31f8b3783d168d0cb2a3fa16ec9c
José Dyego Henrique Cardoso de Sousa <116648398+dyegohrq@users.noreply.github.com>
2024-07-24 17:57:00 -0300

    .navbar

--- assets/script/header.js
+++ assets/script/header.js
@@ -1,7 +1,14 @@
 const burger = document.getElementById('burger');
+const navBar = document.getElementById('nav_bar')
+const btnClose = document.getElementById('btn_close')
 
 burger.addEventListener('click', (e) => {
     e.preventDefault()
 
-    alert('ok')
+    navBar.classList.toggle('active')
 })
+
+btnClose.addEventListener('click', () => {
+    navBar.classList.toggle('active')
+})
+
